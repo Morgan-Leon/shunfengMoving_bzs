d9ced3d7027f8a05199c69ae757766911ea85368
李永楠|Morgan|Léon <14126123@bjtu.edu.cn>
2015-12-02 11:52:22 +0800

    Post方法登录

--- src/main/java/com/lyn/shunfengMoving/controller/user/UserController.java
+++ src/main/java/com/lyn/shunfengMoving/controller/user/UserController.java
@@ -5,8 +5,20 @@ package com.lyn.shunfengMoving.controller.user;
 
 import javax.validation.Valid;
 
+import javassist.NotFoundException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.lyn.shunfengMoving.model.user.User;
+import com.lyn.shunfengMoving.service.user.UserService;
 
 /**
  * @author Morgan-Leon
@@ -16,5 +28,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value="/api")
 public class UserController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+    private UserService service;
+    @Autowired
+    public UserController(UserService service) {
+        this.service = service;
+    }
+    
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    @ResponseBody
+    public User add(@Valid @RequestBody User user) throws Exception {
+    LOGGER.debug("Finding  books in shelf entry with id: {}" );
+		return  service.add(user);
+    }
+    
+    @RequestMapping(value = "/user/{name}", method = RequestMethod.GET)
+    @ResponseBody
+    public User findOne(@PathVariable("name")String name) throws NotFoundException {
+    LOGGER.debug("Finding  books in shelf entry with id: {}" );
+		return  service.findByName(name);
+    }
+    
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    @ResponseBody
+    public User login(@Valid @RequestBody User user) throws NotFoundException {
+    LOGGER.debug("Finding  books in shelf entry with id: {}" );
+		User model =  service.findByName(user.getName());
+		if(!model.getPassword().equals(user.getPassword()))
+			throw new NotFoundException("Wrong Password");
+		
+		return model;
+    }
 
 }
--- src/main/webapp/shunfeng/static/js/login.js
+++ src/main/webapp/shunfeng/static/js/login.js
@@ -7,20 +7,37 @@ $(document).ready(function(){
     var password = $("#password").val();
 
     $(this).attr("disabled", true).text("登录中...");
-    $.ajax({
-      type: 'GET',
-      url: '/api/user/'+username,
-      success:function(){
-        window.location.href="./prototype/index.html";
-      },
-      error:function() {
-        /* Act on the event */
+  //   $.ajax({
+  //     type: 'GET',
+  //     url: '/api/user/'+username,
+  //     success:function(){
+  //       window.location.href="./prototype/index.html";
+  //     },
+  //     error:function() {
+  //       /* Act on the event */
+  //
+  //       alert("用户名密码错误");
+  //       $("#btn-login").attr("disabled", false).text("登录");
+  //     },
+  //
+  // });
+  $.ajax({
+    type: 'POST',
+    url: '/api/login',
+    contentType: "application/json",
+    dataType: "json",
+    data:JSON.stringify({ 'name': username,'password':password}),
+    success:function(){
+      window.location.href="./prototype/index.html";
+    },
+    error:function() {
+      /* Act on the event */
 
-        alert("用户名密码错误");
-        $("#btn-login").attr("disabled", false).text("登录");
-      },
+      alert("用户名密码错误");
+      $("#btn-login").attr("disabled", false).text("登录");
+    },
 
-  });
+});
 });
 
 });
